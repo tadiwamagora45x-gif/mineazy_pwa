500db7a079f517dc25a004baee222d55652ff226
tadiwamagora45x <tadiwamagora45x@gmail.com>
2026-07-11 08:35:17 +0200

    Fix APK to directly load remote URL via WebView

--- android/app/src/main/java/com/mineazy/sales/MainActivity.java
+++ android/app/src/main/java/com/mineazy/sales/MainActivity.java
@@ -1,5 +1,21 @@
 package com.mineazy.sales;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.loadUrl("http://192.168.1.66:3000");
+    }
+}
